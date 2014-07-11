1e57cd089529dbf5ea47ce3bf4c359e80db6baa6
Yash Kin <kubrick@kubrick-EP45-DS3L.(none)>
2014-07-12 00:33:15 +0300

    start

--- src/go/src/huffman/huffman.go
+++ src/go/src/huffman/huffman.go
@@ -2,9 +2,13 @@ package huffman
 
 import (
     "io"
+    "fmt"
+    "bufio"
 )
 
+
 type huffmanNode struct {
+    index int16
     parent *huffmanNode
     left *huffmanNode
     right *huffmanNode
@@ -12,7 +16,7 @@ type huffmanNode struct {
 
 type Huffman struct {
     length uint64
-    encoding *[]string
+    encoding *map[byte]string
     root *huffmanNode
     leaves *[][]huffmanNode
 }
@@ -21,9 +25,41 @@ func New(length uint64) *Huffman {
     return &Huffman{length: length}
 }
 
-func (context *Huffman) Encode(writer *io.Writer) {
+func (self *Huffman) Encode(reader io.Reader) error {
+    if err := self.makeTree(reader); err != nil {
+        return err
+    }
+
+    return nil
 }
 
-func (context *Huffman) Decode(reader *io.Reader) {
+func (self *Huffman) Decode(writer io.Writer) error {
+    return nil
+}
+
+
+func (self *Huffman) makeTree(reader io.Reader) error {
+    rates, obtained, start := make([]int16, 512), uint64(0), 0
+    bReader := bufio.NewReader(reader)
+
+    for obtained < self.length {
+        symbol, err := bReader.ReadByte()
+        if err == nil {
+            index := int(symbol)
+            rates[index] += 1
+            obtained += 1
+
+            if (index < start) {
+                start = index
+            }
+        } else if err == io.EOF {
+            break
+        } else {
+            return err
+        }
+    }
+
+    fmt.Printf("RATES->%v\n", rates)
+    return nil
 }
 
--- src/go/src/main.go
+++ src/go/src/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "huffman"
-import "fmt"
+import (
+    "os"
+    "log"
+    "huffman"
+)
 
 func main() {
+    file, err := os.Open("/home/kubrick/test/file.txt")
+    defer file.Close()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
     huffman := huffman.New(56)
-    fmt.Println(huffman)
+    huffman.Encode(file)
 }
