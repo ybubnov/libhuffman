2f7e2b44fe25a477d71f4aef589a38b5f713e762
Yasha Bubnov <ybubnov@lyft.com>
2022-04-17 13:26:20 +0300

    Build python artefacts (#9)
    This patch builds python wheels and uploads them to PyPi on tag creation.

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ from setuptools import setup
 setup(
     name="huffmanfile",
     version="1.0.0",
-    description="Python bindings for libhuffman",
+
     long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    description="Python bindings for libhuffman",
+
     url="https://github.com/ybubnov/libhuffman",
     author="Chris Copeland",
     author_email="chris@chrisnc.net",
+
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -24,6 +28,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "License :: OSI Approved :: MIT License",
     ],
+
     keywords=["huffman", "encoding", "decoding", "compression"],
     package_dir={"huffmanfile": "huffmanfile"},
     packages=["huffmanfile"],
@@ -31,8 +36,8 @@ setup(
     package_data={"build_tools": ["setup_ffi.py"]},
 
     tests_require=["pytest"],
-    install_requires=["cffi>=1.12.0"],
-    setup_requires=["cffi>=1.12.0", "setuptools_scm"],
+    install_requires=["cffi>=1.15.0"],
+    setup_requires=["cffi>=1.15.0", "setuptools_scm"],
 
     cffi_modules=["setup_ffi.py:ffibuilder"],
 )
