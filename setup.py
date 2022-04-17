3123414b4b9b2e91978999b81276218aea324110
Yasha Bubnov <ybubnov@lyft.com>
2022-04-17 14:09:52 +0300

    Bump library version (#14)
    This patch bumps library version to 1.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="huffmanfile",
-    version="1.0.0",
+    version="1.0.1",
 
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
