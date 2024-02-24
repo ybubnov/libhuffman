5edc87ed2ece62b6e8dd2b217e3b6694d190b2f9
Yasha Bubnov <yasha.bubnov@wolt.com>
2024-02-24 20:58:10 +0100

    Build arm achitechtures as well
    This patch configures ci build to build artefacts for arm architectures.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="huffmanfile",
-    version="1.0.2",
+    version="1.0.3",
 
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
